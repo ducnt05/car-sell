a3acec92e50ff5ef78918e62b449e5848ed8a797
ducnt05 <nahduc05@gmail.com>
2026-02-17 20:23:47 +0700

    update

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+alert("Website đang bảo trì !")
+
 document.getElementById("aboutBtn").addEventListener("click", function () {
+
   window.location.href = "about.html";
 });
 document.addEventListener("click", function() {
