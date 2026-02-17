5cc0f090e6079b7411e6e5be31a418dae6745b4f
ducnt05 <nahduc05@gmail.com>
2026-02-17 15:04:53 +0700

    update

--- main.js
+++
@@ -1,9 +0,0 @@
-function updateClock(){
-    var now = new Date();
-    var hours = String(now.getHours()).padStart(2,"0");
-    var minutes = String(now.getMinutes()).padStart(2,"0");
-    var seconds = String(now.getSeconds()).padStart(2,"0");
-    document.getElementById("clock").innerText = hours + ":" + minutes + ":" + seconds; 
-}
-updateClock();
-setInterval(updateClock,1000);
